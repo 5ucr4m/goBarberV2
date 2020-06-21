4606139d75c6493005330835911f5c50c978e75c
Marcus Vinicius <5ucr4m@gmail.com>
2020-06-21 20:26:06 -0300

    env

--- src/modules/users/services/SendForgotEmailPasswordService.ts
+++ src/modules/users/services/SendForgotEmailPasswordService.ts
@@ -7,7 +7,6 @@ import AppError from '@shared/errors/AppError';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 // import AppError from '@shared/errors/AppError';
 
-
 interface IRequest {
     email: string;
 }
@@ -25,7 +24,7 @@ class SendForgotEmailPasswordService {
         private userTokenRepository: IUserTokensRepository,
     ) {}
 
-    public async execute({ email }: IRequest): Promise<void> { 
+    public async execute({ email }: IRequest): Promise<void> {
         const user = await this.userRepository.findByEmail(email);
 
         if (!user) {
@@ -34,21 +33,26 @@ class SendForgotEmailPasswordService {
 
         const { token } = await this.userTokenRepository.generate(user.id);
 
-        const forgotPasswordTemaplte = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs' )
+        const forgotPasswordTemaplte = path.resolve(
+            __dirname,
+            '..',
+            'views',
+            'forgot_password.hbs',
+        );
 
         await this.mailProvider.sendMail({
             to: {
                 name: user.name,
-                email: user.email
-            }, 
-            subject: "[GoBarber] Recuperação de senha",
+                email: user.email,
+            },
+            subject: '[GoBarber] Recuperação de senha',
             templateData: {
                 file: forgotPasswordTemaplte,
-                variables: { 
+                variables: {
                     name: user.name,
-                    link: `http://localhost:3000/reset_password?token=${token}`
-                }
-            }
+                    link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+                },
+            },
         });
     }
 }
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,8 @@
 import 'reflect-metadata';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
